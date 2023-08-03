3f0a41f08fe2833c661c960c6ff4ad1dfcee9b9f
Andrey S <as111004andr@gmail.com>
2023-08-03 21:24:24 +0700

    Header, Home, About

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+
+
+
+function openMobileMenu() {
+    const mobileMenu = document.getElementById(""); // TODO: Find the element with ...
+    if (mobileMenu.style.display === "block") {
+        mobileMenu.style.display = "none";
+        // TODO: Change image to hamburger icon
+    } else {
+        mobileMenu.style.display = "block";
+        // TODO: Change image to close icon
+    }
+}
+
