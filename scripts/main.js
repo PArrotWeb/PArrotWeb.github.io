02d84eebd75ea014feeec699c658548586742640
Andrey S <as111004andr@gmail.com>
2023-08-08 03:26:43 +0700

    reviews ()code

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    animateElements();
-});
-
-function animateElements() {
-    function onEntry(entry) {
-        entry.forEach(change => {
-            if (change.isIntersecting) {
-                change.target.classList.add('element-show');
-            }
-        });
-    }
-    let options = { threshold: [0.4] };
-    let observer = new IntersectionObserver(onEntry, options);
-    let elements = document.querySelectorAll('.element-animation');
-    for (let elm of elements) {
-        observer.observe(elm);
-    }
-}
 
 function openMobileMenu() {
     const mobileMenu = document.getElementById(""); // TODO: Find the element with ...
@@ -29,3 +10,23 @@ function openMobileMenu() {
     }
 }
 
+document.addEventListener('DOMContentLoaded', function() {
+    animateElements();
+});
+
+function animateElements() {
+    function onEntry(entry) {
+        entry.forEach(change => {
+            if (change.isIntersecting) {
+                change.target.classList.add('element-show');
+            }
+        });
+    }
+
+    let options = {threshold: [0.4]};
+    let observer = new IntersectionObserver(onEntry, options);
+    let elements = document.querySelectorAll('.element-animation');
+    for (let elm of elements) {
+        observer.observe(elm);
+    }
+}
